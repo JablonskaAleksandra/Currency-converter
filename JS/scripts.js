529f0bda8fd9458f1b11775ef1c12fef1ef4c430
Aleksandra Jablonska <jablonska.aleksandra96@gmail.com>
2023-01-20 21:58:48 +0100

    Update JS

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,36 +1,48 @@
 {
-const form = document.querySelector(".js-form");
-const formAmount = document.querySelector(".js-formAmount");
-const formResult = document.querySelector(".js-formResult");
-const formValue = document.querySelector(".js-formValue");
-
-const EUR = 4.6530;
-const GBP = 5.4545;
-const USD = 4.5046;
-const CHF = 4.7685;
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const amount = formAmount.value;
-    const currency = formValue.value;
-    let result = formResult.value;
+const calculateResult = (currency, amount) => {
+    const EUR = 4.6530;
+    const GBP = 5.4545;
+    const USD = 4.5046;
+    const CHF = 4.7685;
 
     switch (currency) {
         case "EUR":
-            result = amount * EUR;
-            break;
+            return amount * EUR;
         case "GBP":
-            result = amount * GBP;
-            break;
+            return amount * GBP;
         case "USD":
-            result = amount * USD;
-            break;
+            return amount * USD;
         case "CHF":
-            result = amount * CHF;
-            break;
-    };
+            return amount * CHF;
 
+    };
+};
+
+const updateResultText = (amount, result, currency) => {
+    const formResult = document.querySelector(".js-formResult");
     formResult.innerText = result.toFixed(2);
-})
-}
+};
+
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+const formAmount = document.querySelector(".js-formAmount");
+const formValue = document.querySelector(".js-formValue");
+
+const amount = +formAmount.value;
+const currency = formValue.value;
+
+const result = calculateResult(currency, amount);
+
+updateResultText(amount, result, currency);
+
+};
+
+const init = () => {
+    const form = document.querySelector(".js-form");
+    form.addEventListener("submit", onFormSubmit);
+};
+
+init();
+
+};
